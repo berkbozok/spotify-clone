29bce925e6aa486585e09ffa1f5ea7028e8c65bf
Berk Bozok <berk_bozok@live.com>
2022-07-14 04:55:59 -0400

    artist and duration added

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -45,10 +45,7 @@ function Sidebar(){
 
                 <div className="space-y-4">
 
-                <button className="flex items-center space-x-2 hover:text-white" onClick={()=>signOut()}> 
-                        
-                        <p>Logout</p>
-                    </button>
+                
 
                     <button className="flex items-center space-x-2 hover:text-white"> 
                         <HomeIcon className="h-5 w-5"/>
