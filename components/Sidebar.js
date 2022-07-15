6ca73c1bab3f857765fec58eabd26970608f3535
Berk Bozok <berk_bozok@live.com>
2022-07-15 00:19:18 -0400

    button color change

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -72,13 +72,13 @@ function Sidebar(){
                         <p>Create Playlist</p>
                     </button>
 
-                    <button className="flex items-center space-x-2  hover:text-white" > 
-                        <HeartIcon className="h-5 w-5"/>
+                    <button className="flex items-center space-x-2  hover:text-white " > 
+                        <HeartIcon className="h-5 w-5  text-blue-500"/>
                         <p>Liked Songs</p>
                     </button>
 
                     <button className="flex items-center space-x-2  hover:text-white"> 
-                        <RssIcon className="h-5 w-5"/>
+                        <RssIcon className="h-5 w-5 text-green-500"/>
                         <p>Your episodes</p>
                     </button>
                     
